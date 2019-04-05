a28ef100c1263af65442f1ecb6f7896fb7b9220c
AJ Larson <aj.larson@devmounta.in>
2019-04-05 12:04:29 -0600

    added structure

---
+++ nightwatch.props.js
@@ -0,0 +1,6 @@
+module.exports = {
+    resourcePath: "/Users/ajlarson/DevMtn/testing-resources/",
+    seleniumServer: "selenium-server-standalone-3.141.59.jar",
+    chromedriver: "chromedriver",
+    geckodriver: "geckodriver"
+}
