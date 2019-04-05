33e1540533daa2656bf77ea9e27e321a59e096ff
Justin Ford <jmford7@gmai.com>
2019-04-05 12:12:36 -0600

    changed login

---
+++ pageObjects.js
@@ -0,0 +1,12 @@
+module.exports = {
+    url: 'https://alpha.yoodlize.com/',
+    elements: {
+        loginStart: {
+            selector: '//*[contains(text(),"Log in")]',
+            locateStrategy: 'xpath'
+        },
+        email: 'input[name="email"]',
+        password: 'input[name="password"]',
+        thisLogin: 'button[type="submit"]',
+    },
+}
