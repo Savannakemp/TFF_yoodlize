7b26e9231240d9dc3bbcdce1c73794a6023ff2eb
Justin Ford <48418933+jmford7@users.noreply.github.com>
2019-04-11 13:42:50 -0600

    Delete nightwatch.props.js

--- nightwatch.props.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    resourcePath: "C:\\Users\\tayme\\OneDrive\\Desktop\\DevMtn-Automation\\Testing-Resources\\",
-    seleniumServer: "selenium-server-standalone-3.141.59.jar",
-    chromedriver: "chromedriver.exe",
-    geckodriver: "geckodriver.exe"
-}
