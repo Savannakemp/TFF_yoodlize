ada3fd3e6a5ae1349cf3666428c40af264b9c4d4
Justin Ford <jmford7@gmai.com>
2019-04-12 11:08:54 -0600

    quick update

--- tests/userTests.js
+++ tests/userTests.js
@@ -1,11 +1,10 @@
 var yoodlizePOM = {}
-var yoodlizeFun = require('../pageObjects/userFun')
+var yoodlizeFun = require('../testAssets/userFun')
 
 module.exports = {
     beforeEach: browser => {
         // browser.url('alpha.yoodlize.com')
         yoodlizePOM = browser.page.yoodlize()
-        yoodlizeFun = browser.page.userFun()
         yoodlizePOM.navigate()
             .waitForElementPresent('input[placeholder="Search for an item"]', 5000)
     },
@@ -13,18 +12,21 @@ module.exports = {
         yoodlizePOM.end()
     },
 
-    //Create New User - Check that no user logged in & new user logged in when complete
-    'Create new user': browser => {
-        yoodlizePOM
-        //email value needs updated prior to each test.
-        .signup('Hugh','Atchinson','Blutoothi8@co.com','Trial123')
-        //need to automate completing the new user popup and logging out?
-    },
+    // //Create New User - Check that no user logged in & new user logged in when complete
+    // 'Create new user': browser => {
+    //     yoodlizePOM
+    //     //email value needs updated prior to each test.
+    //     .signup('Hugh','Atchinson','Blutoothi8@co.com','Trial123')
+    //     //need to automate completing the new user popup and logging out?
+    // },
 
     //Login With New User - Verify that User is logged in
     'Login and verify status': browser => {
         yoodlizePOM
-            .login('UnobtainableMaster@maildrop.cc', 'Trial123')  
+            // .login('UnobtainableMaster@maildrop.cc', 'Trial123')
+            yoodlizeFun(yoodlizePOM, 'UnobtainableMaster@maildrop.cc','Trial123')
+            yoodlizeFun(yoodlizeFun, )
+            
     },
 
     //Modify User Password - Verify that Password modified
